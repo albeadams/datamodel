ca899b67dd82d56ec4fecac92180ea5ee9691f4a
Adams <albeadam@helenachemical.com>
2019-03-15 14:12:58 -0500

    added mods and allow direct table def

--- menu.py
+++ menu.py
@@ -4,21 +4,20 @@ class MainMenu(object):
 
 	def __init__(self):
 		self.oclient = oracle.OracleQuery()
-		self.table_list = 'lookup/table_lookup.txt' 
-
-	def options(self):
-		options = ('\nOptions: \n'
+		self.table_list = 'lookup/table_lookup.txt'
+		self.options = ('\nOptions: \n\n'
 			'   choices                >>>  shows table name choices\n'
-			'   table <table_name>     >>>  lists columns for specific table\n'
+			'   <table_name>           >>>  lists columns for specific table\n'
+			'   table <table_name>     >>>  alternate way to list table columns\n'
 			'   all                    >>>  list all tables\n'
 			'   keys <table_name>      >>>  list keys for specific table\n'
 			'   relat <table_name>     >>>  lists all relationships for specific table\n'
 			'   mod <module_name>      >>>  all tables involved in a module, i.e. sales\n'
 			'   exit/quit/cls          >>>  exit program\n\n'
-			'  Enter: ')
-		return options
+			'  Enter: ') 
 
 	def get_table_columns(self, arg):
+		#using table <table_name>
 		table = ''
 		with open(self.table_list, "r") as fp:
 			for line in iter(fp.readline, ''):
@@ -31,6 +30,27 @@ class MainMenu(object):
 						table = line.split(',')[1]
 		if table:
 			self.oclient.get_table_columns(table)
+			return True
+		else:
+			return False
+
+	def get_table_single(self, arg):
+		#using only <table_name>
+		table = ''
+		with open(self.table_list, "r") as fp:
+			for line in iter(fp.readline, ''):
+				# refactor with contains...
+				if line[:2] == '++':
+					if line.split(',')[0][2:] == arg[0]:
+						table = line.split(',')[1]
+				if line[:1] == '+':
+					if line.split(',')[0][1:] == arg[0]:
+						table = line.split(',')[1]
+		if table:
+			self.oclient.get_table_columns(table)
+			return True
+		else:
+			return False
 
 	def view_table_choices(self, arg):
 		#open file, print off first before comma
@@ -40,18 +60,28 @@ class MainMenu(object):
 					print('    '+line.split(',')[0][2:]+'   [fact]')
 				elif line[:1] == '+':
 					print('    '+line.split(',')[0][1:])
+		return True
 
 	def get_modules(self, arg):
-		if arg[1] == 'sales':
+		if arg[1] == 'sales' or arg[1] == 'inventory':
 			mod = (input('enter ''"monthly"'', ''"daily"'', or ''"transaction"'': ')).lower()
-		mod = (arg[1]+'_'+mod)
+			mod = (arg[1]+'_'+mod)
+		else:
+			mod = arg[1]
 		with open(self.table_list, "r") as fp:
+			flag = False
 			for line in iter(fp.readline, ''):
-				if line[:2] == '++' and line.split(',')[0][2:] == mod:
-					# here, found, but need to loop through following lines:
-					# maybe with a flag that goes until
-					# doesn't find a line with '-'
-					# and for each '-' its a join for this module
+				if not flag and line[:2] == '++' and line.split(',')[0][2:] == mod:
+					# found related table for module
+					flag = True
+					print('  related tables:')
+				elif flag and line[1:2] == '-':
+					key1 = line.split('[')[1][:-1]
+					key2 = line.split('[')[2][:-2]
+					print('\t'+line[2:].split('[')[0]+'  '+''.join(['shared key = ' + key1 if key2 =='*' else 'keys = ' + key1 + ', ' + key2]))
+				else:
+					flag = False
+		return True
 
 	def query_columns(self):
 		return 'columns selected'
@@ -67,8 +97,9 @@ class MainMenu(object):
 	def route(self, args):
 		switch = {
 			'choices': self.view_table_choices,
-			'module': self.get_modules,
+			'mod': self.get_modules,
 			'table': self.get_table_columns,
+			'single': self.get_table_single,
 			'columns': self.query_columns,
 			'keys': self.query_keys,
 			'relationships': self.query_relationships,
@@ -76,19 +107,25 @@ class MainMenu(object):
 
 		if args[0] in switch:
 			func = switch.get(args[0], lambda: 'invalid choice')
-			func(args)
+			hastable = func(args)
+			if not hastable:
+				print('\t***Check spelling and try again***')
 			if args[0] == 'choices': 
 				return True
 			else: 
 				return False
 		else:
-			print('\t***Check spelling and try again***')
+			func = switch.get('single', lambda: 'invalid choice')
+			hastable = func(args)
+			if not hastable:
+				print('\t***Check spelling and try again***')
+
 
 	def run(self):
 		choices = False
 		while True:
 			if not choices:
-				pick = input(self.options())
+				pick = input(self.options)
 			else:
 				pick = input('\n  Enter: ')
 			pick.lower()
--- oracle.py
+++ oracle.py
@@ -5,6 +5,14 @@ class OracleQuery(object):
 
 	def __init__(self):
 		self.connection = oracle.connect(datafig.data['username'], datafig.data['password'], datafig.data['client'])
+		self.options = ('\n\tOptions: \n\n'
+			'\t-------\n'
+			'\t   sample     >>>  show sample\n'
+			'\t   <column>   >>>  show specific column\n'
+			'\t   table      >>>  show table again\n'
+			'\t   detail     >>>  show detailed table\n'
+			'\t   menu       >>>  return to main\n\n'
+			'\t  Enter: ') 
 
 	def disconnect(self):
 		self.connection.close()
@@ -21,14 +29,14 @@ class OracleQuery(object):
 		for eachrow in cursor.description:
 			print('  ' + eachrow[0])
 		while True:
-			seecol = input('\nOptions:\n\tsample    >>> show sample\n\t<column>  >>> show specific column\n\ttable     >>> show table again\n\tdetail    >>> show detailed table\n\tmenu      >>> return to main\n\nEnter: ')
+			seecol = input(self.options)
 			seecol.lower()
 			if seecol == 'menu':
 				break
 			elif seecol == 'table':
 				for eachrow in cursor.description:
 					print('  ' + eachrow[0])
-			elif seecol == 'detailed':
+			elif seecol == 'detail':
 				for eachrow in cursor.description:
 					print('  ' + eachrow[0])
 					print('    ' + '(type: ' + str(eachrow[1]).split('.')[1].split('\'')[0] + '; ' + 'maxsize: ' + str(eachrow[3]) + ''.join(['; ','nullable: ','True)' if str(eachrow[4]) == 'None' else 'Must contain value)']))
@@ -46,11 +54,15 @@ class OracleQuery(object):
 						if seecol == col[0].lower():
 							print()
 							found = True
+							print('  ' + col[0])
+							print('    ' + '(type: ' + str(col[1]).split('.')[1].split('\'')[0] + '; ' + 'maxsize: ' + str(col[3]) + ''.join(['; ','nullable: ','True)' if str(col[4]) == 'None' else 'Must contain value)']))
+							print()
+							print('\t ' + 'First 5 sample:')
 							lines = cursor.fetchmany(numRows=5)
 							for line in lines:
 								if count < 5 and line[index]:
 									count += 1
-									print(line[index])
+									print('\t  '+line[index])
 								else:
 									break
 					if not found:
